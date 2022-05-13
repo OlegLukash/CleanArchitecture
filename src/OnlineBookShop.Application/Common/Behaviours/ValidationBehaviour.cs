b3ad4abde4263da85bb656106c42c127ddaad861
Oleg Lucas <oleg.lucash@gmail.com>
2022-05-13 23:01:52 +0300

    Add Fluent Validator for validating requests.

---
+++ src/OnlineBookShop.Application/Common/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace OnlineBookShop.Application.Common.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                var validationResults = await Task.WhenAll(_validators.Select(v => {
+                    return v.ValidateAsync(context, cancellationToken);
+                }));
+                var failures = validationResults
+                    .Where(r => r.Errors.Any())
+                    .SelectMany(r => r.Errors)
+                    .ToList();
+
+                if (failures.Any())
+                {
+                    var firstFailure = failures.First();
+                    throw new ValidationException(firstFailure.ErrorMessage);
+                }
+            }
+            return await next();
+        }
+    }
+}
--- src/OnlineBookShop.Application/Extensions/ServiceCollectionExtensions.cs
+++ src/OnlineBookShop.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using OnlineBookShop.Application.Common.Behaviours;
 using System.Reflection;
 
 namespace OnlineBookShop.Application.Extensions
@@ -9,7 +11,9 @@ namespace OnlineBookShop.Application.Extensions
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             var assembly = Assembly.GetExecutingAssembly();
+            services.AddValidatorsFromAssembly(assembly);
             services.AddMediatR(assembly);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
 
             return services;
         }
